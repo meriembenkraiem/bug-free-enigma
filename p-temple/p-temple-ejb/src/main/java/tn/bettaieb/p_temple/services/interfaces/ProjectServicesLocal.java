4df442094ab87edaa74c36816e22c23942c87504
bettaieb med ali <medali.bettaieb@esprit.tn>
2016-03-23 20:21:12 +0100

    Signed-off-by: bettaieb med ali <medali.bettaieb@esprit.tn>

--- p-temple/p-temple-ejb/src/main/java/tn/bettaieb/p_temple/services/interfaces/ProjectServicesLocal.java
+++ p-temple/p-temple-ejb/src/main/java/tn/bettaieb/p_temple/services/interfaces/ProjectServicesLocal.java
@@ -1,8 +1,11 @@
 package tn.bettaieb.p_temple.services.interfaces;
 
+import java.util.List;
+
 import javax.ejb.Local;
 
 import tn.bettaieb.p_temple.persistence.Project;
+import tn.bettaieb.p_temple.persistence.User;
 
 @Local
 public interface ProjectServicesLocal {
@@ -11,4 +14,8 @@ public interface ProjectServicesLocal {
 	Boolean assignProjectToUser(String idProject, String idStudent);
 
 	Project findProjectById(String idProject);
+
+	List<Project> findProjectsByUserId(String idUser);
+
+	List<User> findUsersByProjectId(String idProject);
 }
--- p-temple/p-temple-ejb/src/main/java/tn/bettaieb/p_temple/services/interfaces/ProjectServicesRemote.java
+++ p-temple/p-temple-ejb/src/main/java/tn/bettaieb/p_temple/services/interfaces/ProjectServicesRemote.java
@@ -1,8 +1,11 @@
 package tn.bettaieb.p_temple.services.interfaces;
 
+import java.util.List;
+
 import javax.ejb.Remote;
 
 import tn.bettaieb.p_temple.persistence.Project;
+import tn.bettaieb.p_temple.persistence.User;
 
 @Remote
 public interface ProjectServicesRemote {
@@ -11,5 +14,9 @@ public interface ProjectServicesRemote {
 	Boolean assignProjectToUser(String idProject, String idStudent);
 
 	Project findProjectById(String idProject);
+
+	List<Project> findProjectsByUserId(String idUser);
+
+	List<User> findUsersByProjectId(String idProject);
 
 }
